c2f45f57f32f4390a4674800b60efdf4e1723645
Oybek1234567 <davlatovoybek128@gmail.com>
2024-04-01 20:00:25 +0500

    add inventory

--- src/pages/Inventory/index.tsx
+++ src/pages/Inventory/index.tsx
@@ -1,8 +1,25 @@
-
+import { Icons } from "../../constants/icon";
+import ProductDrawer from "./drawer/drawer";
+import Orders from "./orders";
 const Inventory = () => {
-  return (
-    <div>Inventory</div>
-  )
-}
+    return (
+        <div>
+            <div className='w-[424px]'>
+                <input
+                    type='text'
+                    className='w-full h-[44px] bg-[#343743] border-none outline-none pl-[50px] rounded-[20px] ml-[36px]'
+                    placeholder='Search Member'
+                />{" "}
+                <Icons.search className='absolute mt-[-34px] ml-[50px] cursor-pointer' />
+            </div>
+            <div className='w-[126px] h-[44px] bg-[#343743] rounded-[20px] pl-[30px] py-[10px] ml-[930px] mt-[-44px] cursor-pointer'>
+                <span className='absolute pl-[34px] '>Filter</span>{" "}
+                <Icons.filter />
+            </div>
+            <ProductDrawer />
+            <Orders />
+        </div>
+    );
+};
 
-export default Inventory
+export default Inventory;
--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -1,7 +1,7 @@
 import { Icons } from "../constants/icon";
 const Header = () => {
     return (
-        <div className='fixed flex z-10 gap-[26px] mt-[28px] ml-[1150px] cursor-pointer'>
+        <div className='fixed flex z-10 gap-[26px] mt-[38px] ml-[1150px] cursor-pointer'>
             <div className='mt-1.5'>
                 <Icons.UK />
             </div>
